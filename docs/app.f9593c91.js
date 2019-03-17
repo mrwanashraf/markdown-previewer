4fe42094e8707b07b9d7078a01003ce9fd008517
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-03-17 23:41:45 +0200

    remove freecodecamp test script

--- docs/app.f9593c91.js
+++ docs/app.f9593c91.js
@@ -34350,7 +34350,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46767" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41759" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
